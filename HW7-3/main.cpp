e8a7becf6bf929b33d2dca699b6684acee22656a
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-03-28 23:02:29 +0300

    Написали параллельный for_each с возможностью выбора количества потоков

--- HW7-3/main.cpp
+++ HW7-3/main.cpp
@@ -6,9 +6,35 @@
 //
 
 #include <iostream>
+#include <thread>
+#include <future>
+#include <vector>
+#include <numeric>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+template<typename Iterator, class Functor>
+void parallel_for_each(Iterator begin, Iterator end, Functor function, int number_of_threads)
+{
+    const std::size_t length = std::distance(begin, end);
+    const std::size_t block_size = length / number_of_threads;
+    
+    Iterator here = begin;
+    Iterator special_begin = begin;
+    int summary = 0;
+    
+    for (auto i = 0; i < number_of_threads; ++i)
+    {
+        std::advance(here, block_size);
+        
+        std::future < int > here_result = std::async(parallel_for_each< Iterator, Functor >, special_begin, here, function);
+        
+        special_begin = here;
+        
+        summary += here_result.get();
+    }
+}
+
+int main(int argc, const char * argv[])
+{
+    
     return 0;
 }
