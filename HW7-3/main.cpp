b9dfa43340449524d2b63ae83e5d93e21c06590c
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-03-29 00:08:30 +0300

    Великолепие воплоти: было куча ошибок - во-первых, я забыл про рекурсию, во-вторых криво написал async, но теперь все работает идеально

--- HW7-3/main.cpp
+++ HW7-3/main.cpp
@@ -11,30 +11,80 @@
 #include <vector>
 #include <numeric>
 
+template < typename Iterator, class Functor >
+struct for_each_block
+{
+    void operator()(Iterator begin, Iterator end, Functor function)
+    {
+        std::for_each(begin, end, function);
+    }
+};
+
 template<typename Iterator, class Functor>
-void parallel_for_each(Iterator begin, Iterator end, Functor function, int number_of_threads)
+int parallel_for_each(Iterator begin, Iterator end, Functor function, int number_of_threads)
 {
     const std::size_t length = std::distance(begin, end);
     const std::size_t block_size = length / number_of_threads;
     
-    Iterator here = begin;
-    Iterator special_begin = begin;
-    int summary = 0;
+    std::vector < std::thread > threads(number_of_threads);
+    
+    Iterator block_start = begin;
     
     for (auto i = 0; i < number_of_threads; ++i)
-    {
-        std::advance(here, block_size);
-        
-        std::future < int > here_result = std::async(parallel_for_each< Iterator, Functor >, special_begin, here, function);
-        
-        special_begin = here;
-        
-        summary += here_result.get();
-    }
+        {
+            Iterator block_end = block_start;
+            std::advance(block_end, block_size);
+
+            threads[i] = std::thread(
+                for_each_block < Iterator, Functor > (),
+                block_start, block_end, std::ref(function));
+
+            block_start = block_end;
+        }
+    
+    for_each_block < Iterator, Functor > ()(block_start, end, function);
+    
+    std::for_each(threads.begin(), threads.end(),
+            std::mem_fn(&std::thread::join));
+    
+    return 0;
 }
 
 int main(int argc, const char * argv[])
 {
+    std::vector< int > vector(20);
+    
+    std::iota(std::begin(vector), std::end(vector), 11);
+    
+    std::cout << "Here is our data:" << std::endl;
+    
+    int counter = 0;
+    for(auto & element: vector)
+    {
+        std::cout << element << " ";
+        
+        ++counter;
+        if(counter % 4 == 0)
+            std::cout << std::endl;
+    }
+    
+    std::cout << "==========================================" << std::endl << "Let's use the parallel for_each algorithm" << std::endl;
+    
+    int number_of_threads = 0;
+    std::cout << "Enter the number of threads: ";
+    std::cin >> number_of_threads;
+    
+    parallel_for_each(std::begin(vector), std::end(vector), [](auto & element){element += 9;}, number_of_threads);
+    
+    std::cout << "Our new data:" << std::endl;
+    for(auto & element: vector)
+    {
+        std::cout << element << " ";
+        
+        ++counter;
+        if(counter % 4 == 0)
+            std::cout << std::endl;
+    }
     
     return 0;
 }
